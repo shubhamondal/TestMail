f9ff64ef7f7b8755510bcf85d186493a9f9bd8e7
Shubha Mondal <30408428+shubhamondal@users.noreply.github.com>
2019-04-27 21:16:42 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,46 +1,46 @@
 <?php
+var_dump('hiiii');
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\Exception;
+// require 'D:\xampp\composer\vendor\autoload.php'; 
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-require 'D:\xampp\composer\vendor\autoload.php'; 
 
 
+// $first_name = $_POST['first_name'];
+// $last_name = $_POST['last_name'];
+// $email_from = $_POST['email'];
+// $mobile = $_POST['telephone'];
+// $content = $_POST['comments'];
 
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$email_from = $_POST['email'];
-$mobile = $_POST['telephone'];
-$content = $_POST['comments'];
+// $mail = new PHPMailer;
+// $mail->isSMTP();
+// $mail->SMTPDebug = 2;
+// $mail->Host = 'smtp.gmail.com';
+// $mail->Port = 587;
+// $mail->SMTPSecure = 'tls';
+// $mail->SMTPAuth = true;
+// $mail->Username = "mondal.shubha@gmail.com"; //User Name to use for SMTP authentication
+// $mail->Password = "shubhamondal"; //Password to use for SMTP authentication
 
-$mail = new PHPMailer;
-$mail->isSMTP();
-$mail->SMTPDebug = 2;
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = 587;
-$mail->SMTPSecure = 'tls';
-$mail->SMTPAuth = true;
-$mail->Username = "mondal.shubha@gmail.com"; //User Name to use for SMTP authentication
-$mail->Password = "shubhamondal"; //Password to use for SMTP authentication
+// $mail->setFrom($email_from, $first_name. ' '.$last_name);
+// $mail->addReplyTo($email_from, $first_name. ' '.$last_name);
+// $mail->addAddress('mondal.shubha@gmail.com', 'Subha Mondal');
+// $mail->Subject = 'GMail SMTP TEST';
+// $mail->Body = <<<EOT
+// HI,
+// I am {$first_name} {$last_name}.
+// Message: {$content}
+// E-mail: {$email_from}
+// Telephone:{$mobile}
 
-$mail->setFrom($email_from, $first_name. ' '.$last_name);
-$mail->addReplyTo($email_from, $first_name. ' '.$last_name);
-$mail->addAddress('mondal.shubha@gmail.com', 'Subha Mondal');
-$mail->Subject = 'GMail SMTP TEST';
-$mail->Body = <<<EOT
-HI,
-I am {$first_name} {$last_name}.
-Message: {$content}
-E-mail: {$email_from}
-Telephone:{$mobile}
+// Thanks & Regards,
+// {$first_name} {$last_name}
+// EOT;
 
-Thanks & Regards,
-{$first_name} {$last_name}
-EOT;
-
-if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
-}
+// if (!$mail->send()) {
+//     echo "Mailer Error: " . $mail->ErrorInfo;
+// } else {
+//     echo "Message sent!";
+// }
 
 ?>
