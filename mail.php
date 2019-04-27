bd815132778170b810a9d90958758d7077f167d5
Shubha Mondal <mondal.shubha@gmail.com>
2019-04-27 13:20:47 +0530

    first

---
+++ mail.php
@@ -0,0 +1,46 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'D:\xampp\composer\vendor\autoload.php'; 
+
+
+
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email_from = $_POST['email'];
+$mobile = $_POST['telephone'];
+$content = $_POST['comments'];
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 2;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+$mail->Username = ""; //User Name to use for SMTP authentication
+$mail->Password = ""; //Password to use for SMTP authentication
+
+$mail->setFrom($email_from, $first_name. ' '.$last_name);
+$mail->addReplyTo($email_from, $first_name. ' '.$last_name);
+$mail->addAddress('mondal.shubha@gmail.com', 'Subha Mondal');
+$mail->Subject = 'GMail SMTP TEST';
+$mail->Body = <<<EOT
+HI,
+I am {$first_name} {$last_name}.
+Message: {$content}
+E-mail: {$email_from}
+Telephone:{$mobile}
+
+Thanks & Regards,
+{$first_name} {$last_name}
+EOT;
+
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+
+?>
